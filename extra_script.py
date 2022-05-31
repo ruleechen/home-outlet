73e4dceacc052d5bfa88e067c4d1e08779d30a3a
rulee.chen <rulee@live.cn>
2022-05-31 12:43:57 +0800

    fix(build): include `pioEnv` for build deps

--- extra_script.py
+++ extra_script.py
@@ -1,10 +1,11 @@
 Import("env")
 
 # access to global construction environment
-print(env)
+# <SCons.Script.SConscript.SConsEnvironment>
+# print(env.Dump())
 
 def before_build_littlefs(source, target, env):
-  env.Execute("yarn && yarn build")
+  env.Execute("yarn && yarn build --pioEnv=" + env["PIOENV"])
 
 #
 # Custom actions for specific files/objects
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+const yargs = require("yargs");
 const path = require("path");
 const fse = require("fs-extra");
 const del = require("del");
 const gulp = require("gulp");
+
+const {
+  argv: { pioEnv },
+} = yargs.string("pioEnv");
 
 const paths = {
   dist: "data/web",
@@ -15,14 +20,11 @@ function buildDeps() {
   const rootDir = path.resolve(__dirname, "./");
   const libDeps = path.resolve(__dirname, "./.pio/libdeps");
   if (fse.existsSync(libDeps)) {
-    const envNames = fse.readdirSync(libDeps);
-    for (const envName of envNames) {
-      const libDir = path.resolve(libDeps, envName, "home-esp8266/data");
-      if (fse.existsSync(libDir)) {
-        fse.copySync(libDir, path.resolve(rootDir, "data"), {
-          overwrite: true,
-        });
-      }
+    const libDir = path.resolve(libDeps, pioEnv, "home-esp8266/data");
+    if (fse.existsSync(libDir)) {
+      fse.copySync(libDir, path.resolve(rootDir, "data"), {
+        overwrite: true,
+      });
     }
   }
   return Promise.resolve();
